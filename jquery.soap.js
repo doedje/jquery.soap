46edb795a0eeed956530252922148c24dfed862a
Remy Blom <remy.blom@kmt.hku.nl>
2013-10-31 15:03:45 +0100

    preparing 1.2.2

--- jquery.soap.js
+++ jquery.soap.js
@@ -1,6 +1,6 @@
 /*==========================
 jquery.soap.js  http://plugins.jquery.com/soap/ or https://github.com/doedje/jquery.soap
-version: 1.2.1
+version: 1.2.2
 
 jQuery plugin for communicating with a web service using SOAP.
 
