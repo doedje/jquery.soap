c032ae4e592eda115e6e9726ea5c20d440f36c44
Simon <smn@stchr.de>
2014-09-11 17:07:57 +0200

    Encode XML special chars

--- jquery.soap.js
+++ jquery.soap.js
@@ -328,7 +328,7 @@ https://github.com/doedje/jquery.soap/blob/1.4.0/README.md
 			var out = [],
 			    xmlCharMap = {
 		            '<': '&lt;',
-    		        '>': '&gt',
+    		        '>': '&gt;',
     		        '&': '&amp;',
     		        '"': '&quot;',
     		        "'": '&apos;'
